0b2042daee9af8d4d7325aef1131f000f01dff7a
atomicjuice <keemowatson1@gmail.com>
2020-08-18 00:45:42 +0100

    Updates

---
+++ precache-manifest.ff8f74124ced35327e36473c2a9bb8e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18858392313294d58c937094aa53dfdb",
+    "url": "/atomic-app/index.html"
+  },
+  {
+    "revision": "d4f35b1ab19affa0f441",
+    "url": "/atomic-app/static/css/main.a9fff589.chunk.css"
+  },
+  {
+    "revision": "d9bf272738aa3d98f6a6",
+    "url": "/atomic-app/static/js/2.e2911097.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/atomic-app/static/js/2.e2911097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f35b1ab19affa0f441",
+    "url": "/atomic-app/static/js/main.e68bd99c.chunk.js"
+  },
+  {
+    "revision": "1f2568f209d441b851e8",
+    "url": "/atomic-app/static/js/runtime-main.3430a458.js"
+  },
+  {
+    "revision": "7990fc1c9f98c8e58e0fc9f3c8399220",
+    "url": "/atomic-app/static/media/openshot-edit2.7990fc1c.mp4"
+  }
+]);
